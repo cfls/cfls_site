2f197c68dbc6db955f25db854630bae328c6d91b
Leonel Lopez <technoquo@gmail.com>
2026-08-03 11:33:48 +0200

    ocultar interactiv

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -86,12 +86,12 @@
                         Téléchargements gratuits
                     </a>
                  </div>
-                <div role="listitem">
-                    <a wire:navigate href="{{route('interactif-virtuel')}}"
-                       class="block px-4 py-2 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 text-center">
-                        Interactif virtuel
-                    </a>
-                </div>
+{{--                <div role="listitem">--}}
+{{--                    <a wire:navigate href="{{route('interactif-virtuel')}}"--}}
+{{--                       class="block px-4 py-2 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 text-center">--}}
+{{--                        Interactif virtuel--}}
+{{--                    </a>--}}
+{{--                </div>--}}
 
             </div>
         </div>
